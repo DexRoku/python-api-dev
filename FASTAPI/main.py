21a7f67b838f49ab18c6eee8dc7f1463af05023e
Rohith Raju <rohith.raju@ecarxgroup.com>
2024-02-27 13:53:30 +0100

    Adding pydontic models to the code for classification of data in body
    Issue: NONE

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 #Refers to route/path operations.
@@ -14,6 +23,9 @@ def get_posts():
     return {"data": " This is your posts"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f" title: {payload['title']} content: {payload['content']}"}
+def create_posts(new_post: Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data": "new post"}
+
+## we want the user to send title str, content str.
