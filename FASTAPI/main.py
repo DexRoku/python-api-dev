1abe5fa4bfad5ed22adb623a5b99f0c816aaf9a3
Rohith Raju <rohith.raju@ecarxgroup.com>
2024-03-14 17:10:01 +0100

    Adding delete post method to python api

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -17,6 +17,11 @@ def find_post(id):
         if p["id"] == id:
             return p
         
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+        
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
@@ -40,6 +45,15 @@ def get_post(id: int, response: Response):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with {id} not found")
-    #     response.status_code = status.HTTP_404_NOT_FOUND
-    # return {"message": f"post with id : {id} was not found"}
-## we want the user to send title str, content str.
+        
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    #delete post by id
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
+    my_posts.pop(index)
+    return {"message": f"post with id {id} deleted"}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+           
